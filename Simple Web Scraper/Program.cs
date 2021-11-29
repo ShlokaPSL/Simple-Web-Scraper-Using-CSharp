0948c5925344f0028a588952a5d8dabc4e9b3b15
Shloka Jaywant <shloka_jaywant@persistent.com>
2021-11-29 19:30:53 +0530

    Created Simple Web Scraper

---
+++ Simple Web Scraper/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+
+namespace Simple_Web_Scraper
+{
+    class Program
+    {
+        private const string Method = "search";
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter name of city you want to scrape information from : ");
+                var craigslistCity = Console.ReadLine() ?? String.Empty;
+
+                Console.WriteLine("Please enter the CraigsList category that you would like to scrape : ");
+                var craigslsitCategoryName = Console.ReadLine() ?? String.Empty;
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString(
+                        $"http://{craigslistCity.Replace(" ", String.Empty)}.craigslist.org/{Method}/{craigslsitCategoryName}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+
+
+                    var scraper = new Scraper();
+
+                    var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                    if (scrapedElements.Any())
+                    {
+                        foreach (var scrapedElement in scrapedElements)
+                            Console.WriteLine(scrapedElement);
+                    }
+                    else
+                    {
+                        Console.WriteLine("There were no matches found for specified scrape criteria.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SimpleWebScraper.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleWebScraper.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleWebScraper.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07143139-9857-432f-8c96-77370ca8b931")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
